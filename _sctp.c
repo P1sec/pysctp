fa49360ce1d6d6ac11d9dc4572130117c555df66
p1sec <p1sec@vm-pta-dev.(none)>
2012-12-01 20:39:25 +0100

    Now with correct license wording:
    ~/src$ licensecheck -r pysctp/
    pysctp/_sctp.h: LGPL (v2.1 or later)
    pysctp/setup.py: LGPL (v2.1 or later)
    pysctp/_sctp.c: LGPL (v2.1 or later)
    pysctp/sctp.py: LGPL (v2.1 or later)
    pysctp/test_remote_cnx.py: LGPL (v2.1 or later)
    pysctp/test_local_cnx.py: LGPL (v2.1 or later)
    ~/src$

--- _sctp.c
+++ _sctp.c
@@ -2,6 +2,19 @@
  *
  * _sctp.c: C-side bindings
  * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; If not, see <http://www.gnu.org/licenses/>.
+ *
  * Elvis Pfützenreuter (elvis.pfutzenreuter@{gmail.com,indt.org.br})
  * Copyright (c) 2005 Instituto Nokia de Tecnologia
  */
