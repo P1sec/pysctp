3273bf5ceb53f07506b6b6b2acc6a54ebd6dde6c
mark6969 <mark06060909@gmail.com>
2023-06-24 08:19:54 +0000

    ppid Byte-ordering is incorrect fix

--- _sctp.c
+++ _sctp.c
@@ -1663,7 +1663,8 @@ static PyObject* getladdrs(PyObject* dummy, PyObject* args)
 
 static PyObject* sctp_send_msg(PyObject* dummy, PyObject* args)
 {
-	int fd, msg_len, size_sent, ppid, flags, stream, ttl, context;
+	int fd, msg_len, size_sent, flags, stream, ttl, context;
+	int ppid;
 	const char *msg;
 	char *to;
 	int port;
